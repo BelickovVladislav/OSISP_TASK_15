0fc88096424a312320218c456f8ef377dd67a7e0
Belikov Vlad <belickov.vlad1997@yandex.ru>
2018-06-27 16:20:04 +0300

    done

--- OSISP_TASK_15/Form1.cs
+++ OSISP_TASK_15/Form1.cs
@@ -16,5 +16,27 @@ namespace OSISP_TASK_15
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:\\img.png";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var code = e.KeyCode;
+            int distance = 20;
+            switch (code)
+            {
+                case (Keys.Down):
+                case (Keys.Up):
+                    pictureBox1.Top += distance * (code == Keys.Down? 1: -1);
+                    break;
+                case (Keys.Left):
+                case (Keys.Right):
+                    pictureBox1.Left += distance * (code == Keys.Right ? 1 : -1);
+                    break;
+            }
+        }
     }
 }
